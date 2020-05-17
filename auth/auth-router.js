e7af8d8439af2f7a4bac9e77a3558188cc284fa9
Harry Merritt <harry@merritt.es>
2020-05-18 00:49:36 +0100

    Validate request body for required data

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,29 @@
 const router = require("express").Router();
 
-router.post("/register", (req, res) => {
-    // implement registration
+router.post("/register", validateBody(), (req, res) => {
+    // Get user info
+    const { username, password } = req.body;
 });
 
-router.post("/login", (req, res) => {
-    // implement login
+router.post("/login", validateBody(), (req, res) => {
+    // Get user info
+    const { username, password } = req.body;
 });
+
+// Validate required post data
+function validateBody() {
+    return (req, res, next) => {
+        // Check request body exists
+        if (req.body) {
+            // Check required post data exists
+            if (req.body.username && req.body.password) {
+                return next();
+            }
+        }
+
+        // No data sent
+        res.status(400).json({message: "Missing post data"});
+    }
+}
 
 module.exports = router;
