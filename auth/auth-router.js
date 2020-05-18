a59628adc41ca0ff676b94e9e86e97e6365d927c
Harry Merritt <harry@merritt.es>
2020-05-18 01:05:52 +0100

    Create working register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("../api/db");
 
-router.post("/register", validateBody(), (req, res) => {
+router.post("/register", validateBody(), async (req, res, next) => {
     // Get user info
-    const { username, password } = req.body;
+    const user = req.body;
+
+    // Hash user's password
+    user.password = bcrypt.hashSync(user.password, 12);
+
+    try {
+        // Add user to db
+        const [userId] = await db.insert(user);
+
+        // Return username of user
+        res.send({ username: user.username });
+    } catch (err) {
+        next(err);
+    }
 });
 
-router.post("/login", validateBody(), (req, res) => {
+router.post("/login", validateBody(), (req, res, next) => {
     // Get user info
     const { username, password } = req.body;
 });
@@ -22,8 +37,8 @@ function validateBody() {
         }
 
         // No data sent
-        res.status(400).json({message: "Missing post data"});
-    }
+        res.status(400).json({ message: "Missing post data" });
+    };
 }
 
 module.exports = router;
