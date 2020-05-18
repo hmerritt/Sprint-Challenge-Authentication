470c3a05ec5340012382e90a99b3ecde5c833384
Harry Merritt <harry@merritt.es>
2020-05-18 01:43:19 +0100

    Complete tests for login and register

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,12 +1,38 @@
 const request = require("supertest");
 const server = require("../api/server");
 
-// Test data
-const testUser = [
-    { username: "user", password: "pass" },
-];
-
 describe("auth", () => {
+    describe("POST: /register", () => {
+        it("returns JSON type", async () => {
+            const res = await request(server).post("/api/auth/register");
+            expect(res.type).toBe("application/json");
+        });
+
+        it("returns 401 when no body sent", async () => {
+            const res = await request(server).post("/api/auth/register");
+            expect(res.status).toBe(400);
+        });
+
+        it("returns 401 when invalid body sent", async () => {
+            const res = await request(server).post("/api/auth/register").send({notUsername: "bla", notPassword: "bla"});
+            const res2 = await request(server).post("/api/auth/register").send({username: "bla", notPassword: "bla"});
+            const res3 = await request(server).post("/api/auth/register").send({notUsername: "bla", password: "bla"});
+            expect(res.status).toBe(400);
+            expect(res2.status).toBe(400);
+            expect(res3.status).toBe(400);
+        });
+
+        it("returns 200 on valid req-body", async () => {
+            const res = await request(server).post("/api/auth/register").send({username: "bla", password: "bla"});
+            expect(res.status).toBe(200);
+        });
+
+        it("returns username if successful", async () => {
+            const res = await request(server).post("/api/auth/register").send({ username: "user", password: "pass" });
+            expect(res.body.username).toBe("user");
+        });
+    });
+
     describe("POST: /login", () => {
         it("returns JSON type", async () => {
             const res = await request(server).post("/api/auth/login");
@@ -19,12 +45,17 @@ describe("auth", () => {
         });
 
         it("returns 401 when invalid body sent", async () => {
-            const res = await request(server).post("/api/auth/login", {notUsername: "bla", notPassword: "bla"});
-            const res2 = await request(server).post("/api/auth/login", {username: "bla", notPassword: "bla"});
-            const res3 = await request(server).post("/api/auth/login", {notUsername: "bla", password: "bla"});
+            const res = await request(server).post("/api/auth/login").send({notUsername: "bla", notPassword: "bla"});
+            const res2 = await request(server).post("/api/auth/login").send({username: "bla", notPassword: "bla"});
+            const res3 = await request(server).post("/api/auth/login").send({notUsername: "bla", password: "bla"});
             expect(res.status).toBe(400);
             expect(res2.status).toBe(400);
             expect(res3.status).toBe(400);
+        });
+
+        it("returns 200 on valid req-body", async () => {
+            const res = await request(server).post("/api/auth/login").send({ username: "user", password: "pass" });
+            expect(res.status).toBe(200);
         });
     });
 });
